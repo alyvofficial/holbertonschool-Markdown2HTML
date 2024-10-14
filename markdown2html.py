0d7df96b398aa34e7dfc05260ee6e713577548fd
Ali Aliyev <alyvofficial@gmail.com>
2024-10-14 10:13:27 +0400

    Refactor markdown2html.py script to fix missing newline at end of file and ensure executable permissions

--- markdown2html.py
+++ markdown2html.py
@@ -13,8 +13,7 @@ os.chmod(__file__, 0o755)
 if __name__ == '__main__':
     # Test if the number of arguments passed is 2
     if len(sys.argv) != 3:
-        print('Usage: ./markdown2html.py README.md README.html',
-              file=sys.stderr)
+        print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
         sys.exit(1)
 
     # Store the arguments into variables
@@ -31,7 +30,8 @@ if __name__ == '__main__':
         html_content = []
         for line in file_1:
             line = line.rstrip()  # Remove trailing newlines
-            heading = re.match(r'^(#{1,6}) (.*)', line)
+            heading = re.match(r'^(#{1,6}) (.*)', line)  # Match headings
+
             if heading:
                 h_level = len(heading.group(1))  # Number of #
                 content = heading.group(2)  # Heading content
@@ -39,7 +39,8 @@ if __name__ == '__main__':
                     f'<h{h_level}>{content}</h{h_level}>\n'
                 )
             else:
-                html_content.append(f'{line}\n')
+                # Add paragraph or other content
+                html_content.append(f'<p>{line}</p>\n')
 
     # Write the converted HTML content to the output file
     with open(output_file, 'w', encoding='utf-8') as file_2:
